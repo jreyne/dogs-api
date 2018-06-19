41c6c1b9e088b8f44ed97929a782f384fc747f3b
Joshua Paulsen <jpaulsen3110@gmail.com>
2018-06-19 16:31:33 -0400

    my first local commit to the dog-api

---
+++ dogs-api.js
@@ -0,0 +1,10 @@
+// Enviornment Variables
+require("dotenv").config()
+const port = process.env.PORT || 5000
+
+// Dependeceies
+const express = require("express")
+const NodeHTTPError = require("node-http-error")
+const bodyParser = require("body-parser")
+const api = express()
+const {} = require("ramda")
