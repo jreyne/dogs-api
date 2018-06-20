26f460170f59f358b8bae2bb517fa75b96b41652
Will Adkins <wkadki01@gmail.com>
2018-06-20 09:19:26 -0400

    finished dal baseline

--- dal.js
+++ dal.js
@@ -1,9 +1,13 @@
-require("dotenv").config()
-const PouchDB = require("pouchdb-core")
-PouchDB.plugin("pouch-adapter-http")
-const {} = require("ramda")
+require('dotenv').config()
+const PouchDB = require('pouchdb-core')
+PouchDB.plugin('pouch-adapter-http')
+const {} = require('ramda')
 
 // Creates an instance of a couchdb database @ the url provided in the string template
 const db = new PouchDB(
   `${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
 )
+
+const dal = {}
+
+module.exports = dal
