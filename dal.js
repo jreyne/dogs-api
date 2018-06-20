be09affdb17bc71715de51cf15c97b2f6a400b37
Will Adkins <wkadki01@gmail.com>
2018-06-20 09:38:39 -0400

    fixed dal.js baseline in master

--- dal.js
+++ dal.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const PouchDB = require('pouchdb-core')
-PouchDB.plugin('pouch-adapter-http')
+PouchDB.plugin(require('pouchdb-adapter-http'))
 const {} = require('ramda')
 
 // Creates an instance of a couchdb database @ the url provided in the string template
