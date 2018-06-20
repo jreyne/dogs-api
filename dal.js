d5acae4bfe6a56f9c7ac2227c6c6f9c222758fe1
Will Adkins <wkadki01@gmail.com>
2018-06-20 09:37:37 -0400

    Added delete dog endpoint and fixed dal.js baselline

--- dal.js
+++ dal.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const PouchDB = require('pouchdb-core')
-PouchDB.plugin('pouch-adapter-http')
+PouchDB.plugin(require('pouchdb-adapter-http'))
 const {} = require('ramda')
 
 // Creates an instance of a couchdb database @ the url provided in the string template
@@ -8,6 +8,22 @@ const db = new PouchDB(
   `${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
 )
 
-const dal = {}
+const deleteDog = (dogID, callback) => {
+  db.get(dogID, function(err, dog) {
+    if (err) {
+      callback(err)
+      return
+    }
+    db.remove(dog, function(err, deleteResult) {
+      if (err) {
+        callback(err)
+        return
+      }
+      callback(null, deleteResult)
+    })
+  })
+}
+
+const dal = { deleteDog }
 
 module.exports = dal
--- dogs-api.js
+++ dogs-api.js
@@ -1,22 +1,32 @@
 // Enviornment Variables
-require("dotenv").config()
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 // Dependeceies
-const express = require("express")
-const NodeHTTPError = require("node-http-error")
-const bodyParser = require("body-parser")
+const express = require('express')
+const NodeHTTPError = require('node-http-error')
+const bodyParser = require('body-parser')
 const api = express()
-const {} = require("ramda")
+const {} = require('ramda')
+const { deleteDog } = require('./dal')
 
 // Get route
 
-api.get("/", function(req, res, next) {
-  res.status(200).send("Welcome to the dog api dawg.")
+api.get('/', function(req, res, next) {
+  res.status(200).send('Welcome to the dog api dawg.')
 })
 
-api.get("/dogs/:dogID", function(req, res, next) {
+api.get('/dogs/:dogID', function(req, res, next) {
   const dogID = req.params.dogID
 })
 
-api.listen(port, () => console.log("API is up and running.", port))
+api.delete('/dogs/:dogID', function(req, res, next) {
+  deleteDog(req.params.dogID, function(err, data) {
+    if (err) {
+      next(new NodeHTTPError(err.status, err.message, err))
+    }
+    res.status(200).send(data)
+  })
+})
+
+api.listen(port, () => console.log('API is up and running.', port))
