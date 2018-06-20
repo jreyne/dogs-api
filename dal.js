ac3b37b25fa0baf5725ef1c9e32a67bdf4caf396
Joshua Paulsen <jpaulsen3110@gmail.com>
2018-06-20 10:40:10 -0400

    Put features branch

--- dal.js
+++ dal.js
@@ -1,9 +1,31 @@
 require("dotenv").config()
 const PouchDB = require("pouchdb-core")
-PouchDB.plugin("pouch-adapter-http")
+PouchDB.plugin(require("pouchdb-adapter-http"))
 const {} = require("ramda")
 
 // Creates an instance of a couchdb database @ the url provided in the string template
 const db = new PouchDB(
   `${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
 )
+
+const replaceDog = (dogs, callback) => {
+  db.get(dogs._id, function(err, oldDogObj) {
+    if (err) {
+      callback(err)
+      return
+    }
+    db.put(dogs, function(err, replaceResult) {
+      if (err) {
+        callback(err)
+        return
+      }
+      callback(null, replaceResult)
+    })
+  })
+}
+
+const dal = {
+  replaceDog
+}
+
+module.exports = dal
--- dogs-api.js
+++ dogs-api.js
@@ -7,7 +7,14 @@ const express = require("express")
 const NodeHTTPError = require("node-http-error")
 const bodyParser = require("body-parser")
 const api = express()
-const {} = require("ramda")
+const { isEmpty, propOr, curry, difference, keys, not } = require("ramda")
+const createMissingFieldsMsg = require("./lib/create-missing-fields-msg")
+const cleanObj = require("./lib/clean-obj")
+const stringToNumber = require("./lib/string-to-number")
+const stringToBool = require("./lib/string-to-bool")
+
+// Lib files
+const checkRequiredFields = require("./lib/check-required-fields")
 
 // Get route
 
@@ -19,4 +26,39 @@ api.get("/dogs/:dogID", function(req, res, next) {
   const dogID = req.params.dogID
 })
 
+api.put("/dogs/:dogID", function(req, res, next) {
+  const newDog = propOr({}, "body", req)
+
+  if (isEmpty(newDog)) {
+    next(
+      new NodeHTTPError(
+        400,
+        `Missing dog in request body, use a header of 'Content-Type' with a value of 'application/json'.  Be sure to provide valid JSON to represent the cat you wish to add.`
+      )
+    )
+    return
+  }
+
+  const missingFields = checkRequiredFields(["type", "breed"], newBreed)
+  if (not(IsEmpty(missingFields))) {
+    next(new NodeHTTPError(400)`${createMissingFieldsMsg(missingFields)}`)
+    return
+  }
+
+  const cleanDog = cleanObj(["breed", "type"], newDog)
+
+  replaceDog(cleanDog, function(err, data) {
+    if (err) {
+      next(new NodeHTTPError(err.status, err.message, err))
+      return
+    }
+    res.status(200).send(data)
+  })
+})
+
 api.listen(port, () => console.log("API is up and running.", port))
+
+// TODO: DONE No dog in request body, send the 400 response status code and message to the client.
+// TODO: DONE Missing required fields send 400 response
+// TODO: DONE Clean unnecessary prop
+// TODO: id prop value in resource matches the name within in route/path
