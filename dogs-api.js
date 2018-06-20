2261a7bc50fac8d975bd50c5bdf54048a521daa2
JReyne Moore <reyneatwork@gmail.com>
2018-06-20 09:47:40 -0400

    adding POST to api

--- dogs-api.js
+++ dogs-api.js
@@ -1,22 +1,59 @@
 // Enviornment Variables
-require("dotenv").config()
-const port = process.env.PORT || 5000
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 // Dependeceies
-const express = require("express")
-const NodeHTTPError = require("node-http-error")
-const bodyParser = require("body-parser")
-const api = express()
-const {} = require("ramda")
+const express = require('express');
+const NodeHTTPError = require('node-http-error');
+const bodyParser = require('body-parser');
+const api = express();
+const {} = require('ramda');
 
 // Get route
 
-api.get("/", function(req, res, next) {
-  res.status(200).send("Welcome to the dog api dawg.")
-})
+api.get('/', function(req, res, next) {
+	res.status(200).send('Welcome to the dog api dawg.');
+});
 
-api.get("/dogs/:dogID", function(req, res, next) {
-  const dogID = req.params.dogID
-})
+api.get('/dogs/:dogID', function(req, res, next) {
+	const dogID = req.params.dogID;
+});
 
-api.listen(port, () => console.log("API is up and running.", port))
+//POST-CREATE route
+
+api.post('/dogs', function(req, res, next) {
+	const newDog = propOr({}, 'body', req);
+
+	if (isEmpty(newDog)) {
+		next(
+			new NodeHTTPError(
+				400,
+				`There is no dog data in the request.  Please address and resubmit.`
+			)
+		);
+		return;
+	}
+
+	const missingFields = checkRequiredFields(
+		['breed', 'name', 'owner', 'age'],
+		newDog
+	);
+	if (not(isEmpty(missingFields))) {
+		next(
+			new nodeHTTPError(
+				400,
+				`${'You are missing the following fields (missingFields)'}`
+			)
+		);
+		return;
+	}
+
+	const newNewDog = merge(cleanObj(['breed', 'name', 'owner', 'age'], newDog), {
+		id: newDog.name,
+		type: 'dog'
+	});
+	const db = append(newNewDog, db);
+	res.status(201).send({ ok: true, data: newNewDog });
+});
+
+api.listen(port, () => console.log('API is up and running.', port));
