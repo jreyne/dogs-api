681e4d215265cd9eb32f1970b74ea697fef4dd35
Will Adkins <wkadki01@gmail.com>
2018-06-20 10:33:24 -0400

    Added error handling middleware

--- dogs-api.js
+++ dogs-api.js
@@ -1,22 +1,39 @@
 // Enviornment Variables
-require("dotenv").config()
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 // Dependeceies
-const express = require("express")
-const NodeHTTPError = require("node-http-error")
-const bodyParser = require("body-parser")
+const express = require('express')
+const NodeHTTPError = require('node-http-error')
+const bodyParser = require('body-parser')
 const api = express()
-const {} = require("ramda")
+const {} = require('ramda')
+const { deleteDog } = require('./dal')
 
 // Get route
 
-api.get("/", function(req, res, next) {
-  res.status(200).send("Welcome to the dog api dawg.")
+api.get('/', function(req, res, next) {
+  res.status(200).send('Welcome to the dog api dawg.')
 })
 
-api.get("/dogs/:dogID", function(req, res, next) {
+api.get('/dogs/:dogID', function(req, res, next) {
   const dogID = req.params.dogID
 })
 
-api.listen(port, () => console.log("API is up and running.", port))
+// Error Handling Middleware
+
+api.use(function(err, req, res, next) {
+  console.log(
+    'ERROR! ',
+    'METHOD: ',
+    req.method,
+    ' PATH',
+    req.path,
+    ' error:',
+    JSON.stringify(err)
+  )
+  res.status(err.status || 500)
+  res.send(err)
+})
+
+api.listen(port, () => console.log('API is up and running.', port))
